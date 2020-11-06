4b6e346949ae5fa15836f3091032c89aece98bdb
Melody Monarch <oliva.sam.so@gmail.com>
2020-11-06 00:16:36 -0600

    highscore page added

--- Assets/script.js
+++ Assets/script.js
@@ -1,24 +1,29 @@
-var startButton = document.getElementById("startbutton")
-var quizArea = document.getElementById("quizArea")
-var questionsArea = document.getElementById("questionsArea")
-var question = document.getElementById("question")
-var option1 = document.getElementById("option1")
-var option2 = document.getElementById("option2")
-var option3 = document.getElementById("option3")
-var option4 = document.getElementById("option4")
-var showAnswer = document.getElementById("showAnswer")
-var userScoreArea = document.getElementById("userScoreArea")
-var finalScore = document.getElementById("finalScore")
-var userInitials = document.getElementById("userInitials")
-var saveUserInitials = document.getElementById("saveUserInitials")
+var highScores = document.getElementById("highscores");
+var startButton = document.getElementById("startbutton");
+var quizArea = document.getElementById("quizArea");
+var questionsArea = document.getElementById("questionsArea");
+var question = document.getElementById("question");
+var option1 = document.getElementById("option1");
+var option2 = document.getElementById("option2");
+var option3 = document.getElementById("option3");
+var option4 = document.getElementById("option4");
+var showAnswer = document.getElementById("showAnswer");
+var userScoreArea = document.getElementById("userScoreArea");
+var finalScore = document.getElementById("finalScore");
+var userInitials = document.getElementById("userInitials");
+var saveUserInitials = document.getElementById("saveUserInitials");
+var highScoreArea = document.getElementById("highScoreArea");
+var highScoreList = document.getElementById("highScoreList");
+var resetQuizButton = document.getElementById("resetQuizButton");
+var clearScoresButton = document.getElementById("clearScoresButton");
 
-var currentQuestion = 0
-var correctAnswer = 0
-var incorrectAnswer = 0
+var currentQuestion = 0;
+var correctAnswer = 0;
+var incorrectAnswer = 0;
 
-questionsArea.style.display = "none"
-userScoreArea.style.display = "none"
-
+questionsArea.style.display = "none";
+userScoreArea.style.display = "none";
+highScoreArea.style.display = "none";
 
 var questions = [
 {q: "Commonly used data types DO NOT include:",
@@ -44,6 +49,8 @@ answer: 3
 
 function startQuiz() {
     quizArea.style.display = "none"
+    userScoreArea.style.display = "none"
+    highScoreArea.style.display = "none"
     questionsArea.style.display = "block"
     renderQuestion()
 }
@@ -58,7 +65,6 @@ function renderQuestion() {
 
 function checkAnswer(){
     var userChoice = this.getAttribute("data-value")
-    console.log(userChoice)
     if (userChoice == questions[currentQuestion].answer){
         correctAnswer++
         showAnswer.textContent = "✨Correct!✨"
@@ -78,8 +84,27 @@ function checkAnswer(){
     }
 }
 
+function savingInitials(){
+    userScoreArea.style.display = "none"
+    highScoreArea.style.display = "block"
+    var highScoreInitialsEl = document.createElement("li")
+    highScoreInitialsEl.appendChild(userInitials)
+    highScoreList.appendChild(highScoreInitialsEl)
+}
+
+function viewHighScores() {
+    quizArea.style.display = "none"
+    userScoreArea.style.display = "none"
+    questionsArea.style.display = "none"
+    highScoreArea.style.display = "block"
+}
+
+
+highScores.addEventListener("click", viewHighScores)
 startButton.addEventListener("click", startQuiz)
 option1.addEventListener("click", checkAnswer)
 option2.addEventListener("click", checkAnswer)
 option3.addEventListener("click", checkAnswer)
 option4.addEventListener("click", checkAnswer)
+saveUserInitials.addEventListener("click", savingInitials)
+//resetQuizButton.addEventListener("click", startQuiz)
